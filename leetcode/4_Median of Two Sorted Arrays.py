95f66d904cd988f0bcdb2057281cdd6f0b55bc76
Bin Lu <lvbin-3@163.com>
2016-09-13 11:37:22 +0800

    median of two arrays

---
+++ leetcode/4_Median of Two Sorted Arrays.py
@@ -0,0 +1,60 @@
+"""
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+Example 1:
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+author Lu Bin 
+"""
+
+class Solution:
+    # @return a float
+    def findMedianSortedArrays(self, A, B):
+        l=len(A)+len(B)
+        return self.findKth(A,B,l//2) if l%2==1 else (self.findKth(A,B,l//2-1)+self.findKth(A,B,l//2))/2.0
+            
+            
+    def findKth(self,A,B,k):
+        if len(A)>len(B):
+            A,B=B,A
+        if not A:
+            return B[k]
+        if k==len(A)+len(B)-1:
+            return max(A[-1],B[-1])
+        i=len(A)//2
+        j=k-i
+        if A[i]>B[j]:
+            #Here I assume it is O(1) to get A[:i] and B[j:]. In python, it's not but in cpp it is.
+            return self.findKth(A[:i],B[j:],i)
+        else:
+            return self.findKth(A[i:],B[:j],j)
+			
+			
+class Solution1(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        nums1.extend(nums2)
+        combine = sorted(nums1)
+        if len(combine)%2!=0:
+            return combine[len(combine)/2]
+        else:
+            return (combine[len(combine)/2-1]+combine[len(combine)/2])/2.0
+			
+			
+nums1 = [1, 2]
+nums2 = [3, 4]
+s= Solution1()
+print s.findMedianSortedArrays(nums1,nums2)
